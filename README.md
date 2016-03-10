a64a3671ea66aa552e8f8b864de93408e80134bb
Gaurav Kolekar <gaurav.kolekar@mavs.uta.edu>
2016-03-09 20:30:03 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This repository contains the program written for SVM
+
+Run the program: Use matlab to run this program
---
+++ SVMGVK.m
@@ -0,0 +1,7 @@
+%{
+Data Mining Project 1 and 2
+Algorithm Name: Support Vector Machines
+Team Members:
+Gaurav Vivek Kolekar
+Brijesh Dhankara
+%}
